e716cd15d3d63b23f227c6aa32c74bf645c1ff7c
Anshuman Khanna <anshumankh01@gmail.com>
2025-05-24 17:33:18 +0530

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,113 @@
+// In this program we will create a zip() function that is generic i.e. supports multiple types.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define DEFINE_ERROR_STRUCT(TYPE)\
+typedef struct {\
+    TYPE* data;\
+    size_t* size;\
+    char* error;\
+} e_##TYPE;\
+\
+void e_##TYPE##_free(e_##TYPE* ptr) {\
+    if (ptr == NULL) {\
+        return ;\
+    }\
+\
+    free(ptr -> data);\
+    free(ptr -> size);\
+    free(ptr -> error);\
+    free(ptr);\
+}
+
+#define DEFINE_ZIP_ARRAY(TYPE1, TYPE2)\
+typedef struct {\
+    TYPE1* ptr_val1;\
+    TYPE2* ptr_val2;\
+} TYPE1##_##TYPE2##_ZipArray;\
+\
+DEFINE_ERROR_STRUCT(TYPE1##_##TYPE2##_ZipArray)\
+\
+e_##TYPE1##_##TYPE2##_ZipArray* _e_##TYPE1##_##TYPE2##_ZipArray_new(TYPE1##_##TYPE2##_ZipArray* data, size_t* size, char* err) {\
+    e_##TYPE1##_##TYPE2##_ZipArray* result = malloc(sizeof(e_##TYPE1##_##TYPE2##_ZipArray));\
+\
+    result -> data = data;\
+    result -> size = size;\
+    result -> error = err;\
+    \
+    return result;\
+}\
+e_##TYPE1##_##TYPE2##_ZipArray* e_##TYPE1##_##TYPE2##_ZipArray_data(TYPE1##_##TYPE2##_ZipArray* data, size_t* size) {\
+    return _e_##TYPE1##_##TYPE2##_ZipArray_new(data, size, NULL);\
+}\
+e_##TYPE1##_##TYPE2##_ZipArray* e_##TYPE1##_##TYPE2##_ZipArray_error(char* err) {\
+    return _e_##TYPE1##_##TYPE2##_ZipArray_new(NULL, NULL, err);\
+}\
+e_##TYPE1##_##TYPE2##_ZipArray* TYPE1##_##TYPE2##_ZipArray_new(int* arr1, const size_t size1, char* arr2, const size_t size2) {\
+    if (arr1 == NULL || arr2 == NULL || size1 == 0 || size2 == 0) {\
+        return e_##TYPE1##_##TYPE2##_ZipArray_error("No array provided.");\
+    }\
+    \
+    size_t* min_size = malloc(sizeof(size_t));\
+    *min_size = size1 < size2? size1 : size2;\
+\
+    const size_t elem_size1 = sizeof(TYPE1);\
+    const size_t elem_size2 = sizeof(TYPE2);\
+    TYPE1##_##TYPE2##_ZipArray* result = malloc(*min_size * sizeof(TYPE1##_##TYPE2##_ZipArray));\
+\
+    if (result == NULL) {\
+        return e_##TYPE1##_##TYPE2##_ZipArray_error("Failed to assign memory for zipped array.");\
+    }\
+    \
+    for (size_t index = 0; index < *min_size; ++index) {\
+        result[index].ptr_val1 = malloc(elem_size1);\
+\
+        if (result[index].ptr_val1 == NULL) {\
+            return e_##TYPE1##_##TYPE2##_ZipArray_error("Failed to allocate memory for zipped val.");\
+        }\
+\
+        result[index].ptr_val2 = malloc(elem_size2);\
+\
+        if (result[index].ptr_val2 == NULL) {\
+            return e_##TYPE1##_##TYPE2##_ZipArray_error("Failed to allocate memory for zipped val.");\
+        }\
+\
+        memcpy(result[index].ptr_val1, (char*) arr1 + index * elem_size1, elem_size1);\
+        memcpy(result[index].ptr_val2, (char*) arr2 + index * elem_size2, elem_size2);\
+    }\
+\
+    return e_##TYPE1##_##TYPE2##_ZipArray_data(result, min_size);\
+}
+
+DEFINE_ZIP_ARRAY(int, char);
+
+int main() {
+    int* arr1 = calloc(100, sizeof(int));
+    const size_t size1 = 100;
+    char* arr2 = calloc(26, sizeof(char));
+    const size_t size2 = 26;
+
+    for (size_t index = 0; index < 100; ++index) {
+        arr1[index] = (int) index + 1;
+    }
+    for (size_t index = 0; index < 26; ++index) {
+        arr2[index] = (char) index + 'a';
+    }
+
+    e_int_char_ZipArray* result = int_char_ZipArray_new(arr1, size1, arr2, size2);
+
+    if (result -> error != NULL) {
+        perror(result -> error);
+        return 1;
+    }
+
+    for (size_t index = 0; index < *result -> size; ++index) {
+        printf("{ %d, %c }\n", *result -> data[index].ptr_val1, *result -> data[index].ptr_val2);
+    }
+
+    e_int_char_ZipArray_free(result);
+
+    return 0;
+}
